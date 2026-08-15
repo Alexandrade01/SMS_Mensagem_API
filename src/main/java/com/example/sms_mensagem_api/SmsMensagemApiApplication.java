25c553d59872b986ff8ba17506ece8d9e68b82d5
Alexandrade01 <solidwinter96@outlook.com>
2026-08-14 21:59:59 -0300

    first commit

---
+++ src/main/java/com/example/sms_mensagem_api/SmsMensagemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sms_mensagem_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsMensagemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsMensagemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sms_mensagem_api/SmsMensagemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sms_mensagem_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMensagemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
